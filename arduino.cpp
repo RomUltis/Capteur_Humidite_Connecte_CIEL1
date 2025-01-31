59e887099b52b44604509acdfbc8587831213b85
RomUltis <romainultis9090@gmail.com>
2025-01-31 15:23:25 +0100

    arduino v.2

--- arduino.cpp
+++ arduino.cpp
@@ -7,7 +7,9 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(sensorPin);
-  float humidity = map(sensorValue, 0, 1023, 0, 100);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float humidity = (voltage - 0.8) * (100.0 / (3.0 - 0.8));
+  humidity = constrain(humidity, 0, 100);
 
   String json = "{\"sensor\":\"humidity\",\"value\":" + String(humidity, 1) + "}";
   Serial.println(json);
